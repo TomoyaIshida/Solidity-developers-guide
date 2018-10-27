647bd9b8ccbb8f05f8d074ee480451c1f2a2d557
TomoyaIshida <tmyisd@icloud.com>
2018-10-27 22:30:53 +0900

    Apply layout to all pages

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout'
 
 class CampaignNew extends Component {
   render() {
-    return <h2>New Campaign</h2>;
+    return (
+      <Layout>
+        <h2>New Campaign</h2>;
+      </Layout>
+    );
   }
 }
 
