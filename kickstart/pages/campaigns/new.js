a2963c3c6121bbd3a6bd4b73995ee576614b1c37
TomoyaIshida <tmyisd@icloud.com>
2018-10-28 00:56:31 +0900

    Implement form

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout'
 
+import { Form, Button, Input } from 'semantic-ui-react';
+
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  };
+
   render() {
     return (
       <Layout>
-        <h2>New Campaign</h2>;
+        <h2>Create a Campaign</h2>
+
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({ minimumContribution: event.target.value })}
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
