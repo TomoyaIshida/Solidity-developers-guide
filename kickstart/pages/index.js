2ecd7a9ed7efe1b9d6078d7cd7de0974f39d6c1b
TomoyaIshida <tmyisd@icloud.com>
2018-10-27 22:20:39 +0900

    Update pages

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
+
+import { Card, Button } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
+
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    });
+
+    return <Card.Group items={items} />
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <Layout>
+        <div>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
+          <h3>Open Campaigns</h3>
+
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add"
+            primary
+          />
+          {this.renderCampaigns()}
+        </div>
+      </Layout>
+    )
   }
 }
 
