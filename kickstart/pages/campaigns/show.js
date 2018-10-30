93baaf752525dc6e5b62cca036e7eb2d6e0767e3
TomoyaIshida <tmyisd@icloud.com>
2018-10-30 16:00:07 +0900

    Add card to show.js

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 import { Card } from 'semantic-ui-react';
 
@@ -34,6 +35,26 @@ class CampaignShow extends Component {
         meta: 'Address of Manager',
         description: 'The manager created this campaign and can create requests to wihtdraw money',
         style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution(wei)',
+        description: 'You must contribute at least this much wei to become an apporover'
+      },
+      {
+        header: requestCount,
+        meta: 'Number of Requests',
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'Number of people who have already donated to this campaign'
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description: 'The balance is how much money this campaign has left to spend.'
       }
     ];
 
