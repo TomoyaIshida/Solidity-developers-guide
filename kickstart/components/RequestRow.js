c27d2d279f2f05c809a54794521a7d7a32fefae5
TomoyaIshida <tmyisd@icloud.com>
2018-11-01 23:10:51 +0900

    Implement ready to finalize

--- kickstart/components/RequestRow.js
+++ kickstart/components/RequestRow.js
@@ -11,7 +11,7 @@ class RequestRow extends Component {
     await campaign.methods.approveRequest(this.props.id).send({
       from: accounts[0]
     });
-  }
+  };
 
   onFinalize = async () => {
     const campaign = Campaign(this.props.address);
@@ -24,17 +24,35 @@ class RequestRow extends Component {
 
   render() {
     const { Row, Cell } = Table;
-    const { id, request, approversCount} = this.props;
+    const { id, request, approversCount } = this.props;
+    const readyToFinalize = request.approvalCount > approversCount / 2;
 
     return (
-      <Row>
+      <Row
+        disabled={request.complete}
+        positive={readyToFinalize && !request.complete}
+      >
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
         <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
         <Cell>{request.recipient}</Cell>
-        <Cell>{request.approvalCount}/{approversCount}</Cell>
-        <Cell><Button color="green" basic onClick={this.onApprove}>Approve</Button></Cell>
-        <Cell><Button color="teal" basic onClick={this.onFinalize}>Finalize</Button></Cell>
+        <Cell>
+          {request.approvalCount}/{approversCount}
+        </Cell>
+        <Cell>
+          {request.complete ? null : (
+            <Button color="green" basic onClick={this.onApprove}>
+              Approve
+            </Button>
+          )}
+        </Cell>
+        <Cell>
+          {request.complete ? null : (
+            <Button color="teal" basic onClick={this.onFinalize}>
+              Finalize
+            </Button>
+          )}
+        </Cell>
       </Row>
     );
   }
