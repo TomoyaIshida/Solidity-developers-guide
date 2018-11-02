0a3f282299a30574636d4ff8d0c04da3796c3830
TomoyaIshida <tmyisd@icloud.com>
2018-11-02 12:10:02 +0900

    Delete file

--- kickstart/ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
-
-const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  '0xA70bC876C7E6489D9DcAB2E63BCd4b76380054e2'
-);
-
-export default instance;
